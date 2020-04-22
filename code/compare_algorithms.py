8ac79a2a4fa6e4031cf4309c89c9723830cea84a
OBonde <38242118+OBonde@users.noreply.github.com>
2020-04-22 14:19:14 +0200

    gathered data

--- code/compare_algorithms.py
+++ code/compare_algorithms.py
@@ -3,25 +3,24 @@ import matplotlib.pyplot as plt
 import functionlib as flib
 """ Hyperparameters """
 """ SGD, ADAM, CG, L_BFGS"""
-iterations = [1000, 1000, 1000, 1000]
-learn_rate = [0.9, 0.07, 0.1, 0.05]
 N_train = 2048 # number of training observations
+iterations = [1000, 1000, 1000, 1000]
+learn_rate = [0.05, 0.004, 0.1, 0.003]
 batch_size = [64, 128, 256, N_train]
+reps = [1, 1, 3, 20]
 features = 2 # input dimension
-layer_width = [20, 1] # width of layers
+layer_width = [20, 20, 20, 1] # width of layers
 N_layers = len(layer_width) # number of hidden layers
-N_seeds = 10
 random_seed = 1337
+N_seeds = 10
 initial = 1
 """ Generate data """
 np.random.seed(random_seed)
 x = np.random.uniform(0,1,size=(features, N_train)) # each columns is one observations, the row represent features
 y = flib.circles_classification(x)
-
 """ Plot Result """
 plt.figure()
 loss_value = np.zeros(4)
-reps = [1, 1, 3, 20]
 func = [flib.SGD, flib.ADAM, flib.CG, flib.L_BFGS]
 name = ['SGD', 'ADAM', 'CG', 'L-BFGS']
 for i in range(4):
--- code/optimize_hyperparameters.py
+++ code/optimize_hyperparameters.py
@@ -3,34 +3,47 @@ import matplotlib.pyplot as plt
 import functionlib as flib
 """ Hyperparameters """
 """ SGD, ADAM, CG, L_BFGS"""
-iterations = 2000
-learn_rate = 0.05
-features = 2 # input dimension
-batch_size = 2048 
 N_train = 2048 # number of training observations
-layer_width = [20, 1] # width of layers
-N_layers = len(layer_width) # number of hidden l ayers
+iterations = [700, 1000, 700, 1000]
+learn_rate = [0.9, 0.07, 0.1, 0.05]
+batch_size = [64, 128, 256, N_train]
+reps = [1, 1, 3, 20]
+features = 2 # input dimension
+layer_width = [20, 20, 20, 1] # width of layers
+N_layers = len(layer_width) # number of hidden layers
 random_seed = 1337*10
-initial = 1
+N_seeds = 10
+initial = 0.5
 """ Generate data """
 np.random.seed(random_seed)
 x = np.random.uniform(0,1,size=(features, N_train)) # each columns is one observations, the row represent features
 y = flib.circles_classification(x)
 """ Plot Result """
 # parameter to optimize
-batch_size = [512, 1024, 2048] #values to test
-parameter = batch_size  #the parameter you want to change
-loss_value = [0]*len(parameter)
-func = flib.L_BFGS
+learn_rate = [0.006, 0.005, 0.004, 0.003] #values to test
+parameter = learn_rate  #the parameter you want to change
+loss_value = np.zeros(len(parameter))
+func = [flib.SGD, flib.ADAM, flib.CG, flib.L_BFGS]
+name = ['SGD', 'ADAM', 'CG', 'L-BFGS']
 plt.figure()
-n = 5
+k = 3 # choose method
 for i in range(len(parameter)):
-                                                                            # ad index to parameter you want to change
-    weights, biases, loss_list, time_list, gradient_list = func(x, y, layer_width, batch_size[i], learn_rate, iterations, initial, random_seed)
-    loss_value[i] += flib.calculate_loss(weights, biases)
-    flib.plot_loss_time(loss_list, time_list, str(parameter[i]))
-    #flib.plot_loss_epoch(loss_list, iterations//len(loss_list), str(parameter[i]))
-
-print(loss_value)
+    print("parameter ", parameter[i])
+    loss_mean = np.zeros((N_seeds, iterations[k]//reps[k]))
+    time_mean = np.zeros((N_seeds, iterations[k]//reps[k]))
+    j = 0
+    q = 0
+    while j < N_seeds:
+        print("sample ",j)
+        weights, biases, loss_mean[j,:], time_mean[j,:], gradient_list = func[k](x, y, layer_width, batch_size[k], learn_rate[i], iterations[k], initial, random_seed*(j+1)+q)
+        if flib.calculate_loss(weights, biases) < 0.7:
+            j+=1
+        else:
+            q+=1
+            print("q = ",q)
+    #loss_value[i]= flib.calculate_loss(weights, biases)
+    #flib.plot_loss_time(np.mean(loss_mean, axis=0), np.mean(time_mean, axis=0), "Initial " + str(parameter[i]))
+    flib.plot_loss_epoch(np.mean(loss_mean, axis=0), reps[k], str(parameter[i]))
+plt.title(name[k])
 plt.legend()
 plt.show()
